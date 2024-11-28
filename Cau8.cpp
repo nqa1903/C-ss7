db12e79519608c15d1a3bdf4f5bd027f8444bb8f
nqa1903 <103617099+nqa1903@users.noreply.github.com>
2024-11-28 08:26:24 +0700

    Cau8.cpp

--- Cau8.cpp
+++ Cau8.cpp
@@ -6,10 +6,19 @@ int main(){
 	scanf("%d" , &row);
 	printf("Nhap so cot mang 2 chieu : ");
 	scanf("%d", &col);
-	int arr[n][m];
+	int arr[row][col];
 	for(int i = 0 ; i < row ; i++){
-		for(int i = 0 i < col ; i++){
-			printf("Nhap phan tu")
+		for(int j = 0 ; j < col ; j++){
+			printf("Nhap phan tu [%d][%d] : ",i+1,j+1);
+			scanf("%d",&arr[i][j]);
 		}
 	}
+	printf("Mang 2 chieu moi nhap \n");
+	for(int i = 0 ; i < row ; i++){
+		for(int j = 0 ; j < col ; j++){
+			printf("%d \t", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
